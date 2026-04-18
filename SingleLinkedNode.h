bca3073038828d1af321ae11ed1b7e6caf45ecaa
Demyan <musiv31101979@gmail.com>
2026-04-18 19:24:04 +0300

    Add SingleLinkedNode structure for SingleLinkedList

---
+++ SingleLinkedNode.h
@@ -0,0 +1,9 @@
+#ifndef DATA_STRUCTURES_SINGLENODE_H
+#define DATA_STRUCTURES_SINGLENODE_H
+
+struct SingleLinkedNode {
+    int data;
+    SingleLinkedNode* next;
+};
+
+#endif //DATA_STRUCTURES_SINGLENODE_H
